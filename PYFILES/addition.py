46ed7a2a679dfd6fa8778505fe73d5c7bef810b7
anil-k-rajamoni18 <anilkrajamoni99@gmail.com>
2023-12-07 06:09:49 +0530

    added files

---
+++ PYFILES/addition.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 20
+print("addition of two numbers is", num1+num2)
+
+name = "chitra"
+college = "NJIT"
+gpa = 3.9
+print("my name is", name)
+print("iam studing in", college)
+print("my GPA is", gpa)
