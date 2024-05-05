caeeedb93d25d1bdf29b9de5b4ffcf66d2ae9464
anil-k-rajamoni18 <anilkrajamoni99@gmail.com>
2024-05-05 11:22:08 +0530

    updated

---
+++ Python_HandsOn/main.py
@@ -0,0 +1,19 @@
+print(print('hi Chat GPT'))
+
+var = print('hi Azure AI')
+print(var)
+
+
+# Create program called Split Bill Calculcator 
+# Read the bill amount and no of people and tip amount
+# display the split amount for each person
+
+'''
+ex:
+bill_amount : 1000
+no_of_persons: 2
+tip_amount: 10%
+total_bill = bill_amount + (bill_amount * 1/tip_amount)
+each_person = total_bill / no_of_persons
+
+'''
