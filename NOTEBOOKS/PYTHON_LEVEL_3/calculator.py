1eb94fd10b500344f8111a74fb44d3cd3fb3f216
anil-k-rajamoni18 <anilkrajamoni99@gmail.com>
2024-05-12 14:47:03 +0530

    added

--- NOTEBOOKS/PYTHON_LEVEL_3/calculator.py
+++ NOTEBOOKS/PYTHON_LEVEL_3/calculator.py
@@ -1,11 +1,3 @@
-def addition(num1: int, num2: int) -> int:
-    res = num1 + num2 
-    return f'addition of {num1} + {num2} = {res}'
-
-def difference(num1: int, num2: int) -> int:
-    res = num1 - num2 
-    return f'difference of {num1} - {num2} = {res}'
-
-def modulus(num1: int, num2: int) -> int:
-    res = num1 % num2 
-    return f'modulus of {num1} % {num2} = {res}'
+def add(num1: int, num2: int) -> int:
+	res = num1 + num2 
+	return f'Addition of {num1} + '
